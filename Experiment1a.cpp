850edce6a11c798fbb5781d89bdeb889458aedf2
MargadrewNicole <macatangaynics@gmail.com>
2019-03-26 17:54:13 -0700

    1st

--- Experiment1a.cpp
+++ Experiment1a.cpp
@@ -11,7 +11,8 @@ int main ()
     cout << "Sideline       $100.00        28000                 $" << 100.00*28000.00 << endl;
     cout << "Premium        $50.00         35750                 $" << 50.00*35750.00 << endl;
     cout << "Gen. Admission $25.00         18750                 $" << 25.00*18750.00 << endl;
-    cout << "The total ticket sales is $" << (250.00*5750.00)+(100.00*28000.00)+(50.00*35750.00)+(25.00*18750.00) << endl;
+    cout << "The total number of tickets sold is " << 5750+28000+35750+18750 << "." << endl;
+    cout << "The total ticket sales is $" << (250.00*5750.00)+(100.00*28000.00)+(50.00*35750.00)+(25.00*18750.00) << "." << endl;
     _getch();
     return 0;
 }
